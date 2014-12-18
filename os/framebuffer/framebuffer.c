94882c675f87cae84297779a2ac1875315595ff2
fsantanna <francisco.santanna@gmail.com>
2014-12-18 21:44:42 -0200

    (>) (!) working version of FB

--- os/framebuffer/framebuffer.c
+++ os/framebuffer/framebuffer.c
@@ -1,4 +1,21 @@
+#include "assert.h"
 #include "framebuffer.h"
+
+#define PUT8(x,y)  (*((volatile unsigned char*)(x))) = y
+#define GET32(x)   (*((volatile unsigned int*)(x)))
+#define PUT32(x,y) (*((volatile unsigned int*)(x))) = y
+
+#define FB_bpp 24
+#define FB_BPP (FB_bpp/8)
+
+static volatile unsigned int FB[] __attribute__((aligned (16))) = {
+    640, 480,
+    640, 480,
+    0,
+    FB_bpp,
+    0, 0,
+    0, 0
+};
 
 unsigned int MailboxWrite ( unsigned int fbinfo_addr, unsigned int channel )
 {
@@ -34,11 +51,15 @@ unsigned int MailboxRead ( unsigned int channel )
     return(ra&0xfffffff0); // TODO: ra>>4 ??
 }
 
-void DrawPixel(unsigned int x, unsigned int y) {
-    if (x<0 || x>FB[0]) return;
-    if (y<0 || y>FB[1]) return;
-    unsigned int addr = FB[8] + (x + y*FB[0]) * FB_BPP;
-    PUT8(addr+0, 0xFF);
-    PUT8(addr+1, 0xFF);
-    PUT8(addr+2, 0xFF);
+void FB_init (void) { // TODO: parameters
+    MailboxWrite(((unsigned int)FB)+0x40000000, 1);
+    assert(MailboxRead(1) == 0); // TODO: check?
+}
+
+void FB_draw_pixel (unsigned int x, unsigned int y,
+                    unsigned char r, unsigned char g, unsigned char b) {
+    volatile unsigned int addr = FB[8] + (x + y*FB[0]) * FB_BPP;
+    PUT8(addr+0, r);
+    PUT8(addr+1, g);
+    PUT8(addr+2, b);
 }
--- os/framebuffer/framebuffer.h
+++ os/framebuffer/framebuffer.h
@@ -1,20 +1,8 @@
-#define PUT8(x,y)  (*((volatile unsigned char*)(x))) = y
-#define GET32(x)   (*((volatile unsigned int*)(x)))
-#define PUT32(x,y) (*((volatile unsigned int*)(x))) = y
+#ifndef _FRAMEBUFFER_H
+#define _FRAMEBUFFER_H
 
-unsigned int MailboxRead (unsigned int channel );
-unsigned int MailboxWrite (unsigned int fbinfo_addr, unsigned int channel);
-void DrawPixel(unsigned int x, unsigned int y);
-void DrawCharacter (char c, unsigned int x, unsigned int y);
+void FB_init (void);
+void FB_draw_pixel (unsigned int x, unsigned int y,
+                    unsigned char r, unsigned char g, unsigned char b);
 
-#define FB_bpp 24
-#define FB_BPP (FB_bpp/8)
-
-static volatile unsigned int FB[] __attribute__((aligned (16))) = {
-    640, 480,
-    640, 480,
-    0,
-    FB_bpp,
-    0, 0,
-    0, 0
-};
+#endif
