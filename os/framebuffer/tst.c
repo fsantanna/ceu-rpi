a55e476d9f69e24ea3e7e94250bbb83e7889fd9c
fsantanna <francisco.santanna@gmail.com>
2014-12-18 22:11:00 -0200

    (>) os/framebuffer: tst application

--- os/framebuffer/tst.c
+++ os/framebuffer/tst.c
@@ -4,14 +4,14 @@
 void MAIN (void)
 {
     tceu_app* framebuffer = ceu_sys_load((void*)0x110000);
-    //tceu_app* tst         = ceu_sys_load((void*)0x113000);
+    tceu_app* tst         = ceu_sys_load((void*)0x113000);
     assert(framebuffer != NULL);
-    //assert(tst         != NULL);
+    assert(tst         != NULL);
 
-    //ceu_sys_link( tst,1  ,  framebuffer,243 );
+    ceu_sys_link( tst,1  ,  framebuffer,243 );
     //ceu_sys_link( tst,2  ,  framebuffer,242 );
     //ceu_sys_link( tst,3  ,  framebuffer,241 );
 
     ceu_sys_start(framebuffer);
-    //ceu_sys_start(tst);
+    ceu_sys_start(tst);
 }
