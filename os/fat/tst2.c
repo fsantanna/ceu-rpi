66906c8e9b673953f13cf6b7bc61ce9364f65b9c
fsantanna <francisco.santanna@gmail.com>
2014-12-23 11:55:08 -0200

    (+) os/fat tst2 loads os/led from disk

---
+++ os/fat/tst2.c
@@ -0,0 +1,18 @@
+#include "ceu_os.h"
+#include <assert.h>
+
+void MAIN (void)
+{
+    tceu_app* fat = ceu_sys_load((void*)0x110000);
+    tceu_app* tst = ceu_sys_load((void*)0x300000);
+    assert(fat != NULL);
+    assert(tst != NULL);
+
+    ceu_sys_link( tst,1  ,  fat,243 );
+    ceu_sys_link( tst,2  ,  fat,242 );
+    ceu_sys_link( tst,3  ,  fat,241 );
+    ceu_sys_link( tst,4  ,  fat,240 );
+
+    ceu_sys_start(fat);
+    ceu_sys_start(tst);
+}
