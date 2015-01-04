db00644a136f03798912518a376cd7303878632b
fsantanna <francisco.santanna@gmail.com>
2015-01-04 21:07:16 -0200

    (>) os/lua/main using musl w/ -fpic (now also lua-5.1/src)

--- os/lua/other.c
+++
@@ -1,6 +0,0 @@
-int __errno = 0;
-void _exit (int status) {
-}
-
-void _fini (void) {}
-void _sbrk (void) {}
--- os/musl-0.9.15/src/internal/libc.c
+++ os/musl-0.9.15/src/internal/libc.c
@@ -3,19 +3,19 @@
 #ifdef USE_LIBC_ACCESSOR
 struct __libc *__libc_loc()
 {
-	static struct __libc __libc;
+    static struct __libc __libc = {0};
 	return &__libc;
 }
 #else
-struct __libc __libc;
+struct __libc __libc = {0};
 #endif
 
 #ifdef BROKEN_VISIBILITY
 __asm__(".hidden __libc");
 #endif
 
-size_t __hwcap;
-size_t __sysinfo;
+size_t __hwcap = 0;
+size_t __sysinfo = 0;
 char *__progname=0, *__progname_full=0;
 
 weak_alias(__progname, program_invocation_short_name);
